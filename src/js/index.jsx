8a4b9ecb504133101eb6dd46bf1e40f7a9716e43
Chanoch <chanoch@clearbrook.it>
2018-05-17 00:34:37 +0100

    Moved config into index.jsx.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,48 +1,51 @@
-/**
- * TODO do I need to rework this router to support menu ids?
- */
-import React from 'react';
-import {SimpleReactRouter} from '@chanoch/simple-react-router';
+import SimpleReactRouter from '@chanoch/simple-react-router';
 
-import {ListPostsMiddleware} from './posts/redux/ListPosts';
-import {ViewPostMiddleware} from './posts/redux/ViewPost';
-import {rootReducer} from './posts/RootReducer';
-
-import getRoutes from './routes';
-
-var mountpath = 'clearblog';
-
-const routes = getRoutes(mountpath);
 /*
         TODO need to update the express mountpath 
-        TODO need to document the ethos of the concept
-        TODO what if people want to mount it into the root? 
+        TODO need to document the idea
         TODO how to manage top level navigation to non-blog parts? 
-        TODO extract it as an independent component!
-        TODO then use it in a vanilla microsite
         TODO need to resolve css - any others?
         TODO implement with no styling!
 */
+
+// TODO do we need to rework this router to support menu ids?
+import ListPostsPage from './ListPostsPage';
+import ViewPostPage from './ViewPostPage';
+
+// actions, middleware, and the reducer for each action
+import ListPostsAction from './posts/action/ListPostsAction';
+import ViewPostAction from './posts/action/ViewPostAction';
+import ReceivePostsAction from './posts/action/ReceivePostsAction';
+import ReceivePostAction from './posts/action/ReceivePostAction';
+
+console.log(`clearblog/index.jsx:MAKE MOUNTPATH CONFIGURED BY EXPRESS`)
+var mountpath = '/clearblog';
 
 const initialState = {
     posts: [], // the list of blog posts
     post: {}, // the selected article to read in detail
 };
 
+var config = {
+    initialState,
+    actionConfigs: [{
+        initial: true,
+        route: "/",
+        driver: ListPostsAction, 
+        page: (store) => <ListPostsPage store={store} />
+    },{
+        driver: ReceivePostsAction
+    },{
+        route: "/post/",
+        driver: ViewPostAction,
+        page: (store) => <ViewPostPage store={store} />
+    },{
+        driver: ReceivePostAction
+    }]
+};
+
 /**
  * Create store with the redux middleware components which will carry out
  * any mutations required as part of the various actions. 
  */
-const router = new SimpleReactRouter(
-    routes,
-    rootReducer, 
-    initialState, 
-    [ListPostsMiddleware(), ViewPostMiddleware()]); 
-
-/**
- * Fetch the list of blog posts
- */
-import {receivePosts} from './posts/redux/ListPosts';
-import PostService from './posts/service/PostService';
-const postService = new PostService();
-postService.fetchPosts(posts => router.dispatch(receivePosts(posts)));
+new SimpleReactRouter(mountpath, config);
